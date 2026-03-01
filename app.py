71f4cb1dbf84a4cba5c62366c49d173100121692
Walid-Benzine <benzinewalid789@gmail.com>
2026-03-01 12:19:52 +0100

    new

--- app.py
+++ app.py
@@ -325,10 +325,3 @@ def backup():
 def logout():
     session.pop("user", None)
     return redirect("/login")
-
-# ================= RUN =================
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
